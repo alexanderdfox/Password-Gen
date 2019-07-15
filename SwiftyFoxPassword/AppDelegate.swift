f4fdfc9b10b326a7c865f4db7c1ba072b4156cee
Alexander Fox <alex.d.fox@icloud.com>
2019-07-15 01:58:07 -0500

    Initial Commit

---
+++ SwiftyFoxPassword/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftyFoxPassword
+//
+//  Created by Alexander Fox on 7/15/19.
+//  Copyright © 2019 Alexander Fox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftyFoxPassword/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftyFoxPassword
+//
+//  Created by Alexander Fox on 7/15/19.
+//  Copyright © 2019 Alexander Fox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
